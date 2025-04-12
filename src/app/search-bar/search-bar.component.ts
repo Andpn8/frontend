20c93ca200b1a6f71fef78b0bc0ebc6dbbe22826
Andpn8 <andpnace00@gmail.com>
2025-04-12 14:37:43 +0200

    added filtri

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,11 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.scss'
+  styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent {
+  filtersVisible: boolean = false;
+  confirmationVisible: boolean = false;
 
+  toggleFilters() {
+    this.filtersVisible = !this.filtersVisible;
+  }
+
+  // Mostra il messaggio di conferma quando si applicano i filtri
+  showConfirmation() {
+    this.confirmationVisible = true;
+    setTimeout(() => {
+      this.confirmationVisible = false;
+    }, 3000); // Nasconde il messaggio dopo 3 secondi
+  }
+  
 }
+
+
+
