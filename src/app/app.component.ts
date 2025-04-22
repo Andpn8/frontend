5bd5ea9c3950650e8028473422876288c9ceaf39
Andpn8 <andpnace00@gmail.com>
2025-04-22 17:31:42 +0200

    Added components: - footer - login - register - modify-profile - new-announcement - notify - search-history

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { FooterComponent } from "./footer/footer.component";
+import { SearchHistoryComponent } from "./search-history/search-history.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent,SearchBarComponent],
+  imports: [RouterOutlet, NavbarComponent, SearchBarComponent, FooterComponent, SearchHistoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/notify/notify.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notify',
+  imports: [],
+  templateUrl: './notify.component.html',
+  styleUrl: './notify.component.scss'
+})
+export class NotifyComponent {
+
+}
