810349e92bd48eed49d48c4b636201dfef701b99
SEBGAB-00 <seby.sannino0002@gmail.com>
2025-04-23 19:14:44 +0200

    Create footer and begine new-announcement

--- src/app/new-announcement/new-announcement.component.ts
+++ src/app/new-announcement/new-announcement.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-new-announcement',
-  imports: [],
   templateUrl: './new-announcement.component.html',
-  styleUrl: './new-announcement.component.scss'
+  styleUrls: ['./new-announcement.component.scss']
 })
-export class NewAnnouncementComponent {
+export class NewAnnouncementComponent {}
 
-}
