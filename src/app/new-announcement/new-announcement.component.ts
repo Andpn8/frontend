03581c672d6fd333b212aecc360dfbc8fa50fe46
Andpn8 <andpnace00@gmail.com>
2025-04-26 17:13:11 +0200

    added navbar to new announcement

--- src/app/new-announcement/new-announcement.component.ts
+++ src/app/new-announcement/new-announcement.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-new-announcement',
   templateUrl: './new-announcement.component.html',
-  styleUrls: ['./new-announcement.component.scss']
+  styleUrls: ['./new-announcement.component.scss'],
+  imports: [NavbarComponent]
 })
 export class NewAnnouncementComponent {}
 
