e770ae2f8168c80e9d15569bd3b72d83ca54afd5
Andpn8 <andpnace00@gmail.com>
2025-04-26 15:18:00 +0200

    Creating about us section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { NewAnnouncementComponent } from './new-announcement/new-announcement.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomepageComponent },
     { path: 'new-announcement', component: NewAnnouncementComponent},
+    { path: 'about-us', component: AboutUsComponent},
     { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
