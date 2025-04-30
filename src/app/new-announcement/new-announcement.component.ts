ac5e26a7baf7c10c914418cc203d97304de592e0
SEBGAB-00 <seby.sannino0002@gmail.com>
2025-04-30 13:42:54 +0200

    Updates new-announcement and announcement-summary

--- src/app/new-announcement/new-announcement.component.ts
+++ src/app/new-announcement/new-announcement.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from "../navbar/navbar.component";
 import { AnnouncementSummaryComponent } from "../announcement-summary/announcement-summary.component";
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-new-announcement',
   templateUrl: './new-announcement.component.html',
   styleUrls: ['./new-announcement.component.scss'],
-  imports: [NavbarComponent, AnnouncementSummaryComponent]
+  standalone: true,
+  imports: [NavbarComponent, AnnouncementSummaryComponent] // Importazione corretta
 })
-export class NewAnnouncementComponent {}
+export class NewAnnouncementComponent {
+  announcementType: 'vendita' | 'affitto' = 'vendita';
+
+  setAnnouncementType(type: 'vendita' | 'affitto') {
+    this.announcementType = type;
+  }
+}
+
+
+
+
+
 
 
