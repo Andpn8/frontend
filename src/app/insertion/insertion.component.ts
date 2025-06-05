5b8cf8df48a1acf1b8a8b0c2fe6ca2138a3e05c2
Andpn8 <andpnace00@gmail.com>
2025-06-05 11:28:43 +0200

    added insertion component

---
+++ src/app/insertion/insertion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-insertion',
+  imports: [],
+  templateUrl: './insertion.component.html',
+  styleUrl: './insertion.component.scss'
+})
+export class InsertionComponent {
+
+}
