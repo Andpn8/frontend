cfe207bcad45c1320d14198806a976bb9cebcc94
SEBGAB-00 <seby.sannino0002@gmail.com>
2025-06-09 21:20:39 +0200

    updated the recap component

--- src/app/new-announcement-recap/new-announcement-recap.component.ts
+++ src/app/new-announcement-recap/new-announcement-recap.component.ts
@@ -2,20 +2,23 @@ import { Component, Input, ViewChild, ElementRef, AfterViewInit } from '@angular
 import { Router } from '@angular/router';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { AnnouncementSummaryComponent } from "../announcement-summary/announcement-summary.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-announcement-recap',
   standalone: true,
   templateUrl: './new-announcement-recap.component.html',
   styleUrls: ['./new-announcement-recap.component.scss'],
-  imports: [NavbarComponent, AnnouncementSummaryComponent]
+  imports: [NavbarComponent, AnnouncementSummaryComponent,CommonModule]
 })
 export class NewAnnouncementRecapComponent implements AfterViewInit {
 
   @ViewChild('headerContent') headerContent!: ElementRef;
   headerWidth = 0;
-  selectedImage = '';
-  selectedPlanimetria = '';
+    images: string[] = [];
+  selectedImage: string = '';
+  planimetrie: string[] = [];
+  selectedPlanimetria: string = '';
 
   constructor(private router: Router) {} 
 
@@ -30,5 +33,37 @@ export class NewAnnouncementRecapComponent implements AfterViewInit {
 
   goBack(): void {
     this.router.navigate(['/home']); 
+  }
+
+  selectImage(index: number): void {
+    this.selectedImage = this.images[index];
+  }
+
+  prevImage(): void {
+    const currentIndex = this.images.indexOf(this.selectedImage);
+    const prevIndex = currentIndex === 0 ? this.images.length - 1 : currentIndex - 1;
+    this.selectedImage = this.images[prevIndex];
+  }
+
+  nextImage(): void {
+    const currentIndex = this.images.indexOf(this.selectedImage);
+    const nextIndex = currentIndex === this.images.length - 1 ? 0 : currentIndex + 1;
+    this.selectedImage = this.images[nextIndex];
+  }
+
+  selectPlanimetria(index: number): void {
+    this.selectedPlanimetria = this.planimetrie[index];
+  }
+
+  prevPlanimetria(): void {
+    const currentIndex = this.planimetrie.indexOf(this.selectedPlanimetria);
+    const prevIndex = currentIndex === 0 ? this.planimetrie.length - 1 : currentIndex - 1;
+    this.selectedPlanimetria = this.planimetrie[prevIndex];
+  }
+
+  nextPlanimetria(): void {
+    const currentIndex = this.planimetrie.indexOf(this.selectedPlanimetria);
+    const nextIndex = currentIndex === this.planimetrie.length - 1 ? 0 : currentIndex + 1;
+    this.selectedPlanimetria = this.planimetrie[nextIndex];
   }
 }
